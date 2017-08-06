967053fd9c0faf5bff21dff3351f3b28244bb458
David Gilbertson <gilbertson.david@gmail.com>
2017-08-06 12:58:41 +1000

    Set up the project for GitHub pages

--- src/services.js
+++ src/services.js
@@ -1,6 +1,6 @@
 import { filterOutNonNumbers } from './utils';
 
-const API_URL = 'http://frontend-exercise.apps.staging.digital.gov.au/bars';
+const API_URL = 'https://frontend-exercise.apps.staging.digital.gov.au/bars';
 
 export function getAppDataService() {
   return fetch(API_URL)
