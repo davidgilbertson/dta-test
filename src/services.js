fb650ca016e7307ca7b3a8d135c146ac32620c06
David Gilbertson <gilbertson.david@gmail.com>
2017-08-06 12:37:22 +1000

    Create data service

---
+++ src/services.js
@@ -0,0 +1,15 @@
+import { filterOutNonNumbers } from './utils';
+
+const API_URL = 'http://frontend-exercise.apps.staging.digital.gov.au/bars';
+
+export function getAppDataService() {
+  return fetch(API_URL)
+  .then(response => response.json())
+  .then(data => {
+    // sort() alone doesn't sort negatives correctly, hence sort((a, b) => a - b)
+    const buttonValues = filterOutNonNumbers(data.buttons).sort((a, b) => a - b);
+    const barValues = filterOutNonNumbers(data.bars);
+
+    return { buttonValues, barValues };
+  });
+}
